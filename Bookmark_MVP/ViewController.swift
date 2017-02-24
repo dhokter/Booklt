8e254a32e43bbe38ca477c6c4cb68625b2e81929
dletk <dle@macalester.edu>
2017-02-23 20:56:26 -0600

    Reset the project with the new design for tableview

--- Bookmark_MVP/ViewController.swift
+++ Bookmark_MVP/ViewController.swift
@@ -9,20 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var pageNumberTextField: UITextField!
-    @IBOutlet weak var bookTitleTextField: UITextField!
-    
-    // A built-in function, get called when user touch somewhere in the view
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // A built-in function for UITextField, set the done editing status to be true.
-        pageNumberTextField.endEditing(true)
-        bookTitleTextField.isEnabled = false
-        print(bookTitleTextField.state)
-    }
-    // If the user touch the edit button, set the book title to be editable
-    @IBAction func editButtonTouch(_ sender: UIBarButtonItem) {
-        bookTitleTextField.isEnabled = true
-    }
+
 }
 
