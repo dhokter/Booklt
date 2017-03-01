6e0da15f6e2d73ae7d885c4343757b1be4e2ad60
spencermg3 <sgrant1@macalester.edu>
2017-03-01 00:36:29 -0600

    Updated Cartfile to include Realm, and created a new class specifically for Data Storage

---
+++ Bookmark_MVP/DataStorage.swift
@@ -0,0 +1,14 @@
+//
+//  DataStorage.swift
+//  Bookmark_MVP
+//
+//  Created by Spencer Grant on 3/1/17.
+//  Copyright © 2017 Team Grant_le_mandel. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class DataStorage: Object {
+    
+}
