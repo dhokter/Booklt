b1a966a9db015e8a09e8f2b8dd2debf2dee951c6
dletk <dle@macalester.edu>
2017-03-02 13:36:59 -0600

    Add ViewController class for BookDetails, working BACK button

---
+++ Bookmark_MVP/BookDetailsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BookDetailsViewController.swift
+//  Bookmark_MVP
+//
+//  Created by Duc Le on 3/2/17.
+//  Copyright © 2017 Team Grant_le_mandel. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonTouched(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
