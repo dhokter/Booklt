373d36ac85d528d9815b74e78b0f5f3cc3e93c1f
dletk <dle@macalester.edu>
2017-03-22 02:38:21 -0500

    Working GoogleBook API for the Search tab, for the purpose of experimenting

--- Bookmark_MVP/SearchViewController.swift
+++ Bookmark_MVP/SearchViewController.swift
@@ -7,52 +7,94 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class SearchViewController: UIViewController {
-
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var pageCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     @IBAction func searchTouched(_ sender: UIButton) {
-        let title = titleTextField.text;
-        let googleBookSearch = "https://www.googleapis.com/books/v1/volumes?q=\(title)";
-        let url = URL(string: googleBookSearch)
-        let request = URLRequest(url: url!)
+        guard let title = titleTextField.text else {
+            return
+        }
+        let googleBookSearch = "https://www.googleapis.com/books/v1/volumes?q=\(title.replacingOccurrences(of: " ", with: "%20"))"
+        print("--------------> \(googleBookSearch)")
+        let url = URL(string: googleBookSearch)!
+        print("----------> URL is: \(url)")
+        let request = URLRequest(url: url)
         
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         
         let task = session.dataTask(with: request, completionHandler: {(data, response, error) in
-            if error == nil {
+            if error != nil {
+                print("-----------------> Error occur!!!")
                 return
-            } else if data != nil {
-                
             }
-        
+            if data != nil {
+//                let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+//                if let rootDictionary = json as? [String:Any], let items = rootDictionary["items"]  as? [[String:Any]] {
+//                    let volumeInfo = items[0]["volumeInfo"] as? [String: Any]
+//                    let authors = volumeInfo?["authors"] as? [String]
+//                    let page = volumeInfo?["pageCount"] as? Int
+//                    //                    let book = items[0]
+//                    //                    print("----> Keys: \(book.keys)")
+//                    let author = authors?[0]
+//                    print("-------> Author: \(author!)")
+//                    print("-------> Page count: \(page!)")
+//                    self.authorLabel.text = author!
+//                    self.pageCountLabel.text = String(page!)
+//                }
+                //                let json = JSON(data: data!)
+                //                let book = json["items"][0] as? [String:Any]
+                //                print(book["authors"])
+                //                let item = json["items"][0] {
+                //                    if let author = item["authors"] {
+                //                        self.authorLabel.text = author
+                //                    }
+                //                }
+                print("-----> Data is not null")
+                let json = JSON(data: data!)
+                print(json["items"][0]["volumeInfo"]["authors"])
+                let authors = json["items"][0]["volumeInfo"]["authors"].arrayValue.map({$0.stringValue})
+                for author in authors {
+                    print(author)
+                    // Make the code the run on the main thread
+                    DispatchQueue.global().async {
+                        DispatchQueue.main.async {
+                            self.authorLabel.text = author
+                        }
+                    }
+                }
+            }
+            
         })
-        
+        task.resume()
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
