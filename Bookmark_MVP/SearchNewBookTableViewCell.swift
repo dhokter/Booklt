ef863e4b9756f806e57d6c65d651a80dbc0cc754
dletk <dle@macalester.edu>
2017-05-16 16:36:35 +0700

    Create new TableViewController to implement the search online feature

---
+++ Bookmark_MVP/SearchNewBookTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  SearchNewBookTableViewCell.swift
+//  BookIt
+//
+//  Created by Duc Le on 5/16/17.
+//  Copyright © 2017 Team Grant_le_mandel. All rights reserved.
+//
+
+import UIKit
+
+class SearchNewBookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var coverImage: UIImageView!
+    
+    var book: Book? {
+        didSet {
+            if let book = self.book {
+                titleLabel.text = book.title
+                authorLabel.text = book.author
+                // TODO: The cover of the book is now based on color, change it to make it become an image for the purpose of using GoogleBookAPI.
+                // This may need to work with NSData to store the image data into Realm.
+                coverImage.image = iconColor[book.color]
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
