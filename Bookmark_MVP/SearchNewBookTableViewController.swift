97dd841b69dde6d28bfb96aa0cdfc2f578a6831c
dletk <dle@macalester.edu>
2017-05-18 14:54:48 +0700

    Working TableViewController, loaded a list of books for testing. Passed.

--- Bookmark_MVP/SearchNewBookTableViewController.swift
+++ Bookmark_MVP/SearchNewBookTableViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class SearchNewBookTableViewController: UITableViewController {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var books = [Book]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,24 +31,26 @@ class SearchNewBookTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return books.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchNewBookTableViewCell", for: indexPath) as? SearchNewBookTableViewCell else {
+            fatalError("The dequeued cell is not an instance of SearchNewBookTableViewCell")
+        }
+        
         // Configure the cell...
+        cell.book = books[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
